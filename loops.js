58d019c19a153f9db7877cf9d01716a2440313ef
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-26 02:42:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
    for (let i = 0; i < 25; i++){
      if(i === 1){
-       console.log(`I am a strange loop.`);
+       array.push(`I am a strange loop.`);
      }
      else {
-       console.log(`I am ${i} strange loops.`);
+       array.push(`I am ${i} strange loops.`);
      }
 
 }
